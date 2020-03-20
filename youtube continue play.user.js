aac447817b3b9b7c630dd086400827f19a7cb491
奇幻 <30308931+jack850628@users.noreply.github.com>
2020-03-20 14:25:04 +0800

    [修正]註解錯字

--- youtube continue play.user.js
+++ youtube continue play.user.js
@@ -34,7 +34,7 @@
                 )
             ){
                 console.debug('被暫停了，但是我要繼續播放');
-                //ytConfirmDialog.querySelector('yt-button-renderer[dialog-confirm]').click();//當網頁不可見時，出發click是不會繼續播放的，因為要等到網頁可見時觸發UI渲染後才會把對話方塊關掉，對話方塊關掉後才會出發video的play事件
+                //ytConfirmDialog.querySelector('yt-button-renderer[dialog-confirm]').click();//當網頁不可見時，觸發click是不會繼續播放的，因為要等到網頁可見時觸發UI渲染後才會把對話方塊關掉，對話方塊關掉後才會出發video的play事件
                 videoPlay.play();
                 console.debug('按下"是"');
             }else console.debug('對話方塊找不到或是隱藏了',ytConfirmDialog);
