d79be6704bf710e8871f79df1ebe795b7d18a077
奇幻 <jack850628@hotmail.com>
2023-12-07 18:55:06 +0800

    Update youtube continue play.user.js

--- youtube continue play.user.js
+++ youtube continue play.user.js
@@ -8,7 +8,7 @@
 // @description:zh-CN  当出现"影片已暂停，要继续观赏吗？"时忽略它继续播放
 // @description:ja    「動画が一時停止されました。続きを視聴しますか？」が表示されても無視して再生を続けます
 // @namespace          https://greasyfork.org/zh-TW/users/461233-jack850628
-// @version            1.21.231202
+// @version            1.21.231207
 // @author             jack850628
 // @include            https://*.youtube.com/*
 // @noframes
@@ -17,17 +17,15 @@
 // ==/UserScript==
 
 (function() {
-    let pausedFun = function({target: videoPlayer}){
-        console.debug('暫停播放');
-        setTimeout(function(){
-            let ytConfirmDialog = document.querySelector('yt-confirm-dialog-renderer') || document.querySelector('dialog');
+    function searchDialog(videoPlayer){
+        if(videoPlayer.currentTime < videoPlayer.duration){//防止重複播放
+            let ytConfirmDialog = document.querySelector('yt-confirm-dialog-renderer') || document.querySelector('ytmusic-confirm-dialog-renderer') || document.querySelector('dialog');
             if(
                 ytConfirmDialog &&
                 (
                     ytConfirmDialog.parentElement.style.display != 'none' ||
                     (
-                        document.hidden &&
-                        videoPlayer.currentTime < videoPlayer.duration//防止重複播放
+                        document.hidden
                     )//當網頁不可見時，DOM元件不會即時渲染，所以對話方塊的display還會是none
                 )
             ){
@@ -35,14 +33,31 @@
                 //ytConfirmDialog.querySelector('yt-button-renderer[dialog-confirm]').click();//當網頁不可見時，觸發click是不會繼續播放的，因為要等到網頁可見時觸發UI渲染後才會把對話方塊關掉，對話方塊關掉後才會出發video的play事件
                 videoPlayer.play();
                 console.debug('按下"是"');
-            }else console.debug('對話方塊找不到或是隱藏了', ytConfirmDialog && ytConfirmDialog.parentElement, document.hidden, videoPlayer.currentTime, videoPlayer.duration);
-        }, 500);//確保在暫停時對話方塊一定找得到
+            }else{
+                console.debug('對話方塊找不到或是隱藏了', ytConfirmDialog && ytConfirmDialog.parentElement, document.hidden, videoPlayer.currentTime, videoPlayer.duration);
+                if(videoPlayer.paused && videoPlayer.src){
+                    setTimeout(() => searchDialog(videoPlayer), 1000);//再找一次
+                }
+            }
+        }else{
+            console.debug('播放完畢');
+        }
+    }
+    let pausedFun = function({target: videoPlayer}){
+        console.debug('暫停播放');
+        setTimeout(() => searchDialog(videoPlayer), 500);//確保在暫停時對話方塊一定找得到
+    }
+    function setOnPauseEventListener(player){
+        if(!player.dataset.pauseWatcher){
+            player.dataset.pauseWatcher = true;
+            player.addEventListener('pause', pausedFun);
+        }
     }
     function observerPlayerRoot(doc){
-        let nowPlayer = doc.querySelector('video');
-        if(nowPlayer){
-            console.debug('找到播放器', nowPlayer);
-            nowPlayer.addEventListener('pause', pausedFun);
+        let player = doc.querySelector('video');
+        if(player){
+            console.debug('找到播放器', player);
+            setOnPauseEventListener(player);
         }
         let ycpObserver = new MutationObserver((mutationdeList, observer) => {
             mutationdeList.flatMap(i => [...i.addedNodes]).flat().forEach(doc => {
@@ -53,10 +68,9 @@
                     }else if(!["SCRIPT", "STYLE", "LINK", "MATE"].includes(doc.tagName)){
                         player = doc.querySelector('video');
                     }
-                    if(player && player != nowPlayer){
-                        nowPlayer = player;
-                        console.debug('找到播放器', nowPlayer);
-                        nowPlayer.addEventListener('pause', pausedFun);
+                    if(player){
+                        console.debug('找到播放器', player);
+                        setOnPauseEventListener(player);
                     }
                 }
             });
